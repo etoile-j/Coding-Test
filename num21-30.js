41242d1f69dacb19561095ed46b64faf9259a9f1
sujin <soojin9877@naver.com>
2022-05-24 17:26:13 +0900

    num28-num30

--- num21-30.js
+++ num21-30.js
@@ -98,3 +98,68 @@ for(let i=0;i<names.length;i++) {
 }
 
 console.log(obj);
+
+
+/* 28. 2-gram
+2-gram이란 문자열에서 두 개의 연속된 요소를 출력하는 방법입니다.
+예로 'Javascript'를 2-gram으로 반복해 본다면 다음과 같은 결과가 나옵니다.
+입력 : Javascript
+출력
+J a
+a v
+v a
+a s
+s c
+c r
+r i
+i p
+p t
+입력으로 문자열이 주어지면 2-gram으로 출력하는 프로그램을 작성해주세요  */
+
+//나
+function twoGram(input) {
+    const switchArray = input.split('');
+    switchArray.reduce((prev, cur) => {
+        console.log(prev, cur);
+        return cur;
+    })
+}
+
+//답	여기는 for로 찍는 걸 좋아하는 듯 오호
+const data = prompt('문자를 입력하세요.');
+
+for (let i=0; i<data.length-1; i++){
+  console.log(data[i], data[i+1]);
+}
+
+
+/* 29. 대문자만 지나가세요
+알파벳 하나만을 입력하고 그 알파벳이 대문자이면 YES를 아니면 NO를 출력하는 프로그램을 만들어 주세요.     */
+
+//나
+function upperCase(a) {
+    if (a.toUpperCase() === a) {
+        console.log('YES');
+    } else {
+        console.log('NO');
+    }
+}
+
+
+/* 30. 문자열 속 문자 찾기
+문자 pineapple에는 apple이라는 문자가 숨어 있다.
+첫 번째 입력에서는 문자열이 입력되고, 두 번째에는 찾을 문자가 입력되어야 한다.
+그 문자가 시작하는 index를 반환하는 프로그램을 만들어주세요
+ex) 입력 : pinapple is yummy 
+          apple 
+    출력 : 4      */
+
+//나
+const str = prompt('문자열을 입력하세요.');
+const findStr = prompt('찾을 문자열을 입력하세요.');
+
+console.log(str.indexOf(findStr) + 1);
+
+//indexOf() 메서드는 호출한 스트링 객체나 배열에서 
+//주어진 값과 일치하는 값 혹은 요소의 첫 번째 인덱스를 반환하고
+//존재하지 않으면 -1을 반환.
