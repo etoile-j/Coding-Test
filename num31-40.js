961bd7f7e2f10e0aa24583bdf87eaa04aff1134a
sujin <soojin9877@naver.com>
2022-05-27 23:10:53 +0900

    num37

--- num31-40.js
+++ num31-40.js
@@ -113,3 +113,26 @@ for (let i=1; i<=9; i++){
 }
 
 console.log(result);
+
+
+/* 37. 반장 선거
+학생들이 뽑은 후보들을 입력받으면 뽑힌 학생의 이름과 받은 표 수를 출력하는 프로그램 작성.
+ex) 입력 : 원범 원범 혜원 혜원 혜원 혜원 유진 유진
+    출력 : 혜원(이)가 총 4표로 반장이 되었습니다.   */
+
+//나
+const vote = prompt('공백으로 구분해 입력하세요.').split(' ');
+let obj= {};
+
+for (let i = 0; i < vote.length; i++) {
+    if (!obj[vote[i]]) {
+        obj[vote[i]] = + 1;
+    } else {
+        obj[vote[i]]++;
+    }
+}
+
+let winner = '';
+
+winner = Object.keys(obj).reduce((a, b) => obj[a] > obj[b] ? a : b);
+console.log(`${winner}(이)가 총 ${obj[winner]}표로 반장이 되었습니다.`);
