a65d8c27f4b49c3d026571a0c3a5d70bda2b9be1
sujin <soojin9877@naver.com>
2022-09-05 15:49:40 +0900

    num52

--- num51-60.js
+++ num51-60.js
@@ -1,4 +1,4 @@
-/* 50. merge sort
+/* 51. merge sort
 다음 코드의 빈칸을 채워 병합정렬을 완성해 봅시다. */
 //시간복잡도 O(N*logN)
 function mergeSort(arr) {
@@ -39,3 +39,30 @@ const array = prompt('배열을 입력하세요')
     .map((n) => parseInt(n, 10));
 
 console.log(mergeSort(array));
+
+/* 52. quick sort
+다음 빈 칸을 채워 퀵 정렬을 완성해주세요. */
+function quickSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+    const pivot = arr[0];
+    const left = [];
+    const right = [];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < pivot) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
+        }
+    }
+    return quickSort(left).concat(pivot, quickSort(right));
+}
+
+const array = prompt('배열을 입력하세요')
+    .split(' ')
+    .map((n) => parseInt(n, 10));
+
+console.log(quickSort(array));
