8fe233886e9b7fb9d4c1e1af631bd1451f502470
sujin <soojin9877@naver.com>
2022-09-14 10:04:22 +0900

    num57-num58

--- num51-60.js
+++ num51-60.js
@@ -171,3 +171,32 @@ const key = Object.keys(nationWidth);
 const value = Object.values(nationWidth).map((i) => Math.abs(i - korea));
 const minIndex = value.indexOf(Math.min(...value));
 console.log(key[minIndex], value[minIndex]);
+
+/* 57. 1의 개수
+0부터 1000까지 1의 개수를 세는 프로그램을 만들려고 합니다.
+예를 들어 0부터 20까지 1의 개수를 세어본다면 1, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19에
+각각 1이 들어가므로 12개의 1이 있게 됩니다. 11은 1이 2번 들어간 셈이죠.
+
+그렇다면 0부터 1000까지의 수에서 1은 몇 번이나 들어갔을까요? 출력해 주세요. */
+
+//나
+let box = '';
+for (let i = 0; i <= 1000; i++) {
+    box += i;
+}
+
+let count = 0;
+for (let j of box) {
+    if (j == 1) {
+        count += 1;
+    }
+}
+console.log(count);
+
+/* 58. 콤마 찍기
+숫자를 입력받고 천 단위로 콤마(,)를 찍어주세요.
+예를 들어, 123456789를 입력받았으면 123,456,789를 출력해야 합니다. */
+
+//나
+const input = parseInt(prompt('숫자를 입력해주세요'));
+console.log(input.toLocaleString('ko-KR'));
