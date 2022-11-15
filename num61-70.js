51a8b5548b8bb3b916888a63fa249912d6c0eb39
sujin <soojin9877@naver.com>
2022-11-15 23:00:11 +0900

    num68

--- num61-70.js
+++ num61-70.js
@@ -205,3 +205,44 @@ function sol(n) {
 
 const handshakeCount = 59;
 console.log(sol(handshakeCount));
+
+/* 68. 버스 시간표
+버스 시간표와 현재 시간이 주어졌을 때 버스 도착 시간이 얼마나 남았는지 알려주는 프로그램을 만들어주세요.
+- 버스 시간표와 현재 시간이 입력으로 주어집니다.
+- 출력 포맷은 "00시 00분"입니다. 만약 1시간 3분이 남았다면 "01시간 03분"으로 출력해야 합니다.
+- 버스 시간표에 현재 시간보다 이전인 버스가 있다면 "지나갔습니다."라고 출력합니다. */
+
+// 입력
+// ["12:30", "13:20", "14:13"]
+// "12:40"
+// 출력
+// ['지나갔습니다', '00시간 40분', '01시간 33분']
+
+//나
+function arrival(time, bus) {
+    let print = [];
+    const current = new Date(`2022/11/15/${time}`);
+    for (let i = 0; i < bus.length; i++) {
+        let target = new Date(`2022/11/15/${bus[i]}`);
+        let gap =
+            target.getHours() * 60 +
+            target.getMinutes() -
+            (current.getHours() * 60 + current.getMinutes());
+        if (gap < 0) {
+            print.push('지나갔습니다.');
+        } else {
+            print.push(
+                `${Math.trunc(gap / 60)
+                    .toString()
+                    .padStart(2, '0')}시간 ${(gap % 60)
+                    .toString()
+                    .padStart(2, '0')}분`
+            );
+        }
+    }
+    return console.log(print);
+}
+
+const bus = ['12:30', '13:20', '14:13'];
+const time = '12:40';
+arrival(time, bus);
