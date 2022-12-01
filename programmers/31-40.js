f7bb447065cf680bed3b51db77922909e226d806
sujin <soojin9877@naver.com>
2022-12-01 11:03:14 +0900

    33-36번

--- programmers/31-40.js
+++ programmers/31-40.js
@@ -18,3 +18,88 @@ function solution2(n) {
     }
     return answer;
 }
+
+// Day 9. 수학, 문자열, 해시, 완전탐색, 조건문
+//33. 개미 군단
+function solution3(hp) {
+    switch (hp % 5) {
+        case 0:
+            return hp / 5;
+        case 1:
+        case 3:
+            return Math.floor(hp / 5) + 1;
+        case 2:
+        case 4:
+            return Math.floor(hp / 5) + 2;
+    }
+}
+
+//34. 모스부호 (1)
+function solution4(letter) {
+    const morse = {
+        '.-': 'a',
+        '-...': 'b',
+        '-.-.': 'c',
+        '-..': 'd',
+        '.': 'e',
+        '..-.': 'f',
+        '--.': 'g',
+        '....': 'h',
+        '..': 'i',
+        '.---': 'j',
+        '-.-': 'k',
+        '.-..': 'l',
+        '--': 'm',
+        '-.': 'n',
+        '---': 'o',
+        '.--.': 'p',
+        '--.-': 'q',
+        '.-.': 'r',
+        '...': 's',
+        '-': 't',
+        '..-': 'u',
+        '...-': 'v',
+        '.--': 'w',
+        '-..-': 'x',
+        '-.--': 'y',
+        '--..': 'z',
+    };
+    var answer = '';
+    const arr = letter.split(' ');
+    for (let i of arr) {
+        answer += morse[i];
+    }
+    return answer;
+}
+
+//35. 가위 바위 보
+function solution5(rsp) {
+    var answer = '';
+
+    for (let i of rsp) {
+        switch (i) {
+            case '0':
+                answer += '5';
+                break;
+            case '2':
+                answer += '0';
+                break;
+            case '5':
+                answer += '2';
+        }
+    }
+    return answer;
+}
+
+//36. 구슬을 나누는 경우의 수
+function solution6(balls, share) {
+    function fac(num) {
+        let n = BigInt(num);
+        if (n <= BigInt(0)) {
+            return BigInt(1);
+        } else {
+            return n * fac(n - BigInt(1));
+        }
+    }
+    return fac(balls) / (fac(balls - share) * fac(share));
+}
