f10ba6e5a6a2f9ae2fd73a8ef736ffeaef1ed102
sujin <soojin9877@naver.com>
2023-01-02 19:59:30 +0900

    93-96번

--- programmers/Lv.0/91-100.js
+++ programmers/Lv.0/91-100.js
@@ -23,3 +23,44 @@ function solution2(id_pw, db) {
     }
     return 'fail';
 }
+
+// Day 24. 수학, 시뮬레이션, 문자열, 조건문, 반복문
+//93. 치킨 쿠폰
+function solution3(chicken) {
+    let answer = 0;
+
+    while (chicken >= 10) {
+        answer += parseInt(chicken / 10);
+        chicken = parseInt(chicken / 10) + (chicken % 10);
+    }
+
+    return answer;
+}
+
+//94. 이진수 더하기
+function solution4(bin1, bin2) {
+    const add = parseInt(bin1, 2) + parseInt(bin2, 2);
+    return add.toString(2);
+}
+
+//95. A로 B 만들기
+function solution5(before, after) {
+    return [...before].sort().join('') === [...after].sort().join('') ? 1 : 0;
+}
+
+//96. k의 개수
+function solution6(i, j, k) {
+    var answer = '';
+    let count = 0;
+
+    for (let b = i; b <= j; b++) {
+        answer += b;
+    }
+
+    for (let a = 0; a < answer.length; a++) {
+        if (answer[a] == k) {
+            count += 1;
+        }
+    }
+    return count;
+}
