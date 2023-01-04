c1ff71c0b147a75bbad31ab49acd23e33f1e7846
sujin <soojin9877@naver.com>
2023-01-04 19:42:46 +0900

    230104 풀이

---
+++ programmers/Lv.1/자릿수_더하기.js
@@ -0,0 +1,8 @@
+function solution(n) {
+    const num = n.toString();
+    let answer = 0;
+    for (let i = 0; i < num.length; i++) {
+        answer += parseInt(num[i]);
+    }
+    return answer;
+}
