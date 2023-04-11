2f69370d151afa14789f9a6bdc16eb05593f3189
sujin <soojin9877@naver.com>
2023-04-11 15:00:50 +0900

    20230411

--- programmers/Lv.2/기능개발.js
+++ programmers/Lv.2/기능개발.js
@@ -27,3 +27,23 @@ function solution(progresses, speeds) {
 
     return answer;
 }
+
+function solution2(progresses, speeds) {
+    let answer = [0];
+    let days = progresses.map((progress, idx) =>
+        Math.ceil((100 - progress) / speeds[idx])
+    );
+    let maxDay = days[0];
+
+    for (let i = 0, j = 0; i < days.length; i++) {
+        if (days[i] <= maxDay) {
+            answer[j] += 1;
+        } else {
+            maxDay = days[i];
+            answer[++j] = 1;
+        }
+    }
+
+    return answer;
+}
+// 며칠 걸리는지 미리 계산
