33fda03fda4316e26a93c3b32a1a2fd62354737e
sujin <soojin9877@naver.com>
2023-05-11 21:36:27 +0900

    20230511

--- programmers/Lv.2/튜플.js
+++ programmers/Lv.2/튜플.js
@@ -10,3 +10,26 @@ function solution(s) {
     );
     return answer;
 }
+
+function solution2(s) {
+    return s
+        .slice(2, -2)
+        .split('},{')
+        .map((nums) => nums.split(',').map((num) => Number(num)))
+        .sort((a, b) => a.length - b.length)
+        .reduce(
+            (acc, cur) => [...acc, ...cur.filter((val) => !acc.includes(val))],
+            []
+        );
+}
+
+// s를 배열(모양)로 바꿔주는 다른 방법
+//1.
+JSON.parse(s.replace(/{/g, '[').replace(/}/g, ']'));
+
+//2.
+s.slice(2, -2).split('},{');
+//후에 숫자로 매핑
+
+//3.
+s.match(/{[\d,]+}/g);
