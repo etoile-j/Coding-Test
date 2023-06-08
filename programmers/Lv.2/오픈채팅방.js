4d737fe3a286805120f9a609baa85aedc8ee5af3
sujin <soojin9877@naver.com>
2023-06-08 13:57:48 +0900

    20230608

--- programmers/Lv.2/오픈채팅방.js
+++ programmers/Lv.2/오픈채팅방.js
@@ -16,3 +16,23 @@ function solution(record) {
     }
     return answer;
 }
+
+// Map 버전
+function solution2(record) {
+    let answer = [];
+    const lastNickname = new Map();
+
+    record.forEach((v) => {
+        let [_, id, nickname] = v.split(' ');
+        if (nickname) lastNickname.set(id, nickname);
+    });
+
+    record.forEach((v) => {
+        let [type, id] = v.split(' ');
+        if (type === 'Enter')
+            answer.push(`${lastNickname.get(id)}님이 들어왔습니다.`);
+        if (type === 'Leave')
+            answer.push(`${lastNickname.get(id)}님이 나갔습니다.`);
+    });
+    return answer;
+}
