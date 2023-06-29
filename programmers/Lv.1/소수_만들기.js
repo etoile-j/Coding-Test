aff245acd38cb17622c6db70d72a456177579667
sujin <soojin9877@naver.com>
2023-06-29 17:18:06 +0900

    20230629

---
+++ programmers/Lv.1/소수_만들기.js
@@ -0,0 +1,20 @@
+function solution(nums) {
+    let answer = 0;
+
+    function check(num) {
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i === 0) return false;
+        }
+        return true;
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            for (let k = j + 1; k < nums.length; k++) {
+                let sum = nums[i] + nums[j] + nums[k];
+                if (check(sum)) answer++;
+            }
+        }
+    }
+    return answer;
+}
