cba213dd0d1bcd98e05c91bf1f6d1474c98cdf6f
sujin <soojin9877@naver.com>
2023-07-18 11:38:54 +0900

    20230718

--- programmers/Lv.2/124 나라의 숫자.js
+++ programmers/Lv.2/124 나라의 숫자.js
@@ -11,3 +11,14 @@ function solution(n) {
     }
     return answer.split('').reverse().join('');
 }
+
+function solution2(n) {
+    const nation = [4, 1, 2];
+    let result = '';
+
+    while (n) {
+        result = nation[n % 3] + result;
+        n = Math.floor((n - 1) / 3);
+    }
+    return result;
+}
