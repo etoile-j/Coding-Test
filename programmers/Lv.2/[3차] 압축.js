d4d24d363c90821e46a4e77a7d367851fc64a469
sujin <soojin9877@naver.com>
2023-07-25 14:38:57 +0900

    20230725

--- programmers/Lv.2/[3차] 압축.js
+++ programmers/Lv.2/[3차] 압축.js
@@ -22,3 +22,25 @@ function solution(msg) {
     }
     return answer;
 }
+
+function solution2(msg) {
+    let answer = [];
+    const dic = [''];
+    for (let i = 65; i < 91; i++) {
+        dic.push(String.fromCharCode(i));
+    }
+    let w = '';
+
+    for (let j = 0; j < msg.length; j++) {
+        w += msg[j];
+
+        if (!dic.includes(w)) {
+            answer.push(dic.indexOf(w.slice(0, -1)));
+            dic.push(w);
+
+            w = msg[j];
+        }
+    }
+    if (w) answer.push(dic.indexOf(w));
+    return answer;
+}
