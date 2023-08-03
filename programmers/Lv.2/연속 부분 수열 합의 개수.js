d5f16aea2af43888ab5c492b11204537d8d478e2
sujin <soojin9877@naver.com>
2023-08-03 15:46:40 +0900

    20230803

--- programmers/Lv.2/연속 부분 수열 합의 개수.js
+++ programmers/Lv.2/연속 부분 수열 합의 개수.js
@@ -1,15 +1,28 @@
 function solution(elements) {
-    let answer = 0;
     const circle = elements.concat(elements);
     const set = new Set();
+
     for (let i = 1; i < elements.length; i++) {
         for (let j = 0; j < elements.length; j++) {
-            const Sumlength = circle.slice(j, j + i).reduce((acc, cur) => {
-                return (acc += cur);
-            }, 0);
-            set.add(Sumlength);
+            set.add(
+                circle.slice(j, j + i).reduce((acc, cur) => (acc += cur), 0)
+            );
         }
     }
-    answer = set.size + 1;
-    return answer;
+
+    return set.size + 1;
 }
+
+function solution2(elements) {
+    const circle = elements.concat(elements);
+    const set = new Set();
+    for (let i = 0; i < elements.length; i++) {
+        let sum = 0;
+        for (let j = 0; j < elements.length; j++) {
+            sum += circle[i + j];
+            set.add(sum);
+        }
+    }
+    return set.size;
+}
+// 각 원소를 시작점으로 하나씩 더해가는 방법
