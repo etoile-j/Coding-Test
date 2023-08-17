b711a3716f1982bd5f9d98b875b82cb960f4ced0
sujin <soojin9877@naver.com>
2023-08-17 16:00:59 +0900

    20230817

--- programmers/Lv.2/다리를 지나는 트럭.js
+++ programmers/Lv.2/다리를 지나는 트럭.js
@@ -16,3 +16,24 @@ function solution(bridge_length, weight, truck_weights) {
     }
     return time;
 }
+
+function solution2(bridge_length, weight, truck_weights) {
+    let time = 0,
+        bridge = [[0, 0]], // [트럭 무게, 트럭이 다리를 빠져나가는 시간]
+        bridgeWeights = 0;
+
+    while (bridge.length > 0 || truck_weights.length > 0) {
+        if (bridge[0][1] === time) bridgeWeights -= bridge.shift()[0];
+
+        if (bridgeWeights + truck_weights[0] <= weight) {
+            bridgeWeights += truck_weights[0];
+            bridge.push([truck_weights.shift(), time + bridge_length]);
+        } else {
+            if (bridge[0]) time = bridge[0][1] - 1;
+        }
+        time++;
+    }
+    return time;
+}
+// 각 트럭이 다리를 건너는 데 걸리는 시간을 미리 계산해
+// 다음 트럭이 당장 다리에 못 올라올 때 다리 건널 수 있는 시간으로 건너감
