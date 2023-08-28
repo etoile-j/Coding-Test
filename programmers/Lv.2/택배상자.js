ac41a9e76a88968646a2cde31df8a6e6911c5910
sujin <soojin9877@naver.com>
2023-08-28 23:17:16 +0900

    20230828

---
+++ programmers/Lv.2/택배상자.js
@@ -0,0 +1,13 @@
+function solution(order) {
+    let answer = 0;
+    const stack = [];
+
+    for (let i = 1; i <= order.length; i++) {
+        stack.push(i);
+        while (stack.length && stack[stack.length - 1] === order[answer]) {
+            stack.pop();
+            answer++;
+        }
+    }
+    return answer;
+}
