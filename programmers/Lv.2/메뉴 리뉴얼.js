bc9cf1d2c9d664d6ab03a6460402270ac7154b04
sujin <soojin9877@naver.com>
2023-09-04 21:50:07 +0900

    20230904

--- programmers/Lv.2/메뉴 리뉴얼.js
+++ programmers/Lv.2/메뉴 리뉴얼.js
@@ -41,3 +41,34 @@ function dfs(arr, selectNumber) {
     });
     return results;
 }
+
+function solution2(orders, course) {
+    const ordered = {};
+    const candidates = {};
+    const max = Array(10 + 1).fill(0);
+    const dfs = (arr, start, len, foods) => {
+        if (len === 0) {
+            ordered[foods] = (ordered[foods] || 0) + 1;
+            if (ordered[foods] > 1) candidates[foods] = ordered[foods];
+            max[foods.length] = Math.max(max[foods.length], ordered[foods]);
+            return;
+        }
+
+        for (let i = start; i < arr.length; i++) {
+            dfs(arr, i + 1, len - 1, foods + arr[i]);
+        }
+    };
+
+    orders.forEach((od) => {
+        const sorted = od.split('').sort();
+        course.forEach((len) => {
+            dfs(sorted, 0, len, '');
+        });
+    });
+
+    const answer = Object.keys(candidates).filter(
+        (food) => max[food.length] === candidates[food]
+    );
+
+    return answer.sort();
+}
